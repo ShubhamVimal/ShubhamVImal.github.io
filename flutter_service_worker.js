093cfb4c1eafd9488743d747a589fc734e14e6c5
Shubham Vimal <51742764+ShubhamVimal@users.noreply.github.com>
2021-07-08 10:39:12 +0530

    Minor update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -112,9 +112,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "5109af664685288763f9dd8a38ecaffa",
-"/": "5109af664685288763f9dd8a38ecaffa",
-"main.dart.js": "810666bed16d5cf57057088146250302",
+"index.html": "8adb9dc2f4b5399218afbcb3f29652c8",
+"/": "8adb9dc2f4b5399218afbcb3f29652c8",
+"main.dart.js": "5aa6c9c41b63571ee464fd8e598a9a35",
 "manifest.json": "4494413cde58ee458a7b4e00e4230c7a",
 "version.json": "98ece6ba2f545a3f872d2df2184a4c1d"
 };
